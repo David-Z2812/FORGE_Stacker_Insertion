7dda7c15eab916acde587bd7eac0e80030f4f745
David-Z2812 <david.zipperstein@tum.de>
2025-10-15 13:19:32 +0200

    Change ppo config to 1024 envs Adjust initial hand position for improved grasping in ForgeStackerInsert

--- source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory/factory_env.py
@@ -484,7 +484,7 @@ class FactoryEnv(DirectRLEnv):
             # print("DEBUG_KP target_held_base_pos:", target_held_base_pos[idx].cpu().numpy())
             # print("DEBUG_KP keypoints_held[0]:", keypoints_held[idx].cpu().numpy())
             # print("DEBUG_KP keypoints_fixed[0]:", keypoints_fixed[idx].cpu().numpy())
-            a0, b0 = self.cfg_task.keypoint_coef_baseline
+            # a0, b0 = self.cfg_task.keypoint_coef_baseline
             # print("DEBUG_KP squash a0,b0:", a0, b0, "keypoint_scale:", self.cfg_task.keypoint_scale)
 
         rew_dict = {
